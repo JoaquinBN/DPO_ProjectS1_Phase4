ef8c22d2b93c06ebb16521e635b9766ddd9a7c99
albert-mr <58224660+albert-mr@users.noreply.github.com>
2022-07-02 05:11:22 +0200

    d

--- src/PersistenceLayer/TrialsCSVManager.java
+++ src/PersistenceLayer/TrialsCSVManager.java
@@ -14,18 +14,8 @@ import java.util.List;
 
 public class TrialsCSVManager implements TrialsFileManager {
     private static final String filename = "files/Trials.csv";
-    private CSVWriter writer;
-    private CSVReader reader;
 
     public TrialsCSVManager(){
-        try {
-            this.reader = new CSVReader(new FileReader(filename));
-            this.writer = new CSVWriter(new FileWriter(filename, true),
-                    CSVWriter.DEFAULT_SEPARATOR,
-                    CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.NO_ESCAPE_CHARACTER, "\n");
-        } catch (IOException e) {
-            // handle exception
-        }
     }
 
     /**
@@ -35,6 +25,9 @@ public class TrialsCSVManager implements TrialsFileManager {
      */
     @Override
     public void writeTrials(ArrayList<Trials> trials) throws IOException {
+        CSVWriter writer = new CSVWriter(new FileWriter(filename, false),
+                CSVWriter.DEFAULT_SEPARATOR,
+                CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.NO_ESCAPE_CHARACTER, "\n");
             for (Trials trial : trials){
                 writer.writeNext(trial.getDataToWrite());
             }
@@ -48,6 +41,7 @@ public class TrialsCSVManager implements TrialsFileManager {
      */
     @Override
     public List<String[]> readTrials() throws IOException, CsvException {
+        CSVReader reader = new CSVReader(new FileReader(filename));
             List<String[]> trials = reader.readAll();
             reader.close();
             return trials;
